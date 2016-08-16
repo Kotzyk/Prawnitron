7d99ef6297ccd5527221680e0fa3f6a0784beccd
Mateusz Koziestański <matico575@gmail.com>
2016-08-16 15:51:36 +0200

    Update app.routing.ts

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,23 +3,18 @@
  */
 import {Routes, RouterModule} from '@angular/router';
 
-import {PrawnitronComponent} from './prawnitron.component';
-import {NotComponent} from "./not.component";
-import {SearchFormComponent} from "./search-form.component";
-import {AboutComponent} from "./about.component";
+import {SearchComponent} from 'app/search/search.component';
+import {NotComponent} from "app/not.component";
+import {AboutComponent} from "app/about.component";
 
 const appRoutes: Routes = [
     {
-        path: 'prawnitron',
-        component: PrawnitronComponent
+        path: 'app',
+        component: SearchComponent
     },
     {
         path: '404',
         component: NotComponent
-    },
-    {
-        path: 'search',
-        component: SearchFormComponent
     },
     {
         path: 'about',
