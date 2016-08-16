5642537ec64f4abb03808ad1329e84abb799544f
Mateusz Koziestański <matico575@gmail.com>
2016-08-16 15:43:54 +0200

    Update not.component.ts
    zalinkowano css zamiast wprost do HTML

--- app/not.component.ts
+++ app/not.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'not-found',
-    templateUrl: '/app/not.component.html'
+    templateUrl: '/app/not.component.html',
+    stylesUrl: 'app/not.component.css'
 })
 export class NotComponent {
 }
