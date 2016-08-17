40c598fbe89bfce06f9e9fa75c0b092bf5052ff4
Mateusz Koziestański <matico575@gmail.com>
2016-08-17 13:32:30 +0200

    stub
    stub

--- app/search/search-results-table.component.ts
+++ app/search/search-results-table.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
+import {IUstawa} from 'app/ustawa'
 
 @Component({
-  selector: 'results-table'
+  selector: 'results-table',
   templateUrl: 'app/search/search-resuts-table.component.html'
   
 })
-export class SearchTableResultsComponent { }
+export class SearchTableResultsComponent { 
+  ustawy: IUstawa[]=[
+    
+  ];
+}
