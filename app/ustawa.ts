3d720d9752a0b7e43d9e5481bad0c6eda06027af
Mateusz Koziestański <matico575@gmail.com>
2016-08-17 09:37:55 +0200

    Update ustawa.ts
    Ustawa => IUstawa

--- app/ustawa.ts
+++ app/ustawa.ts
@@ -1,9 +1,14 @@
 /**
  * Created by Mateusz on 2016-08-13.
  */
-export class Ustawa {
-
-    nazwa: string;
-    data: string;
-    id: number;
+export class IUstawa {
+    tytul_skrocony: string;
+    dokument_id: number;
+    content_url: string;
+    data_obowiazywania: string;
+    zrodlo: string;
+    status: string;
+    rok: number;
+    nr: number;
+    poz: number;
 }
