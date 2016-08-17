ff1c96dc61545003b8e71bf42bc3517078dc7376
Mateusz Koziestański <matico575@gmail.com>
2016-08-17 10:42:51 +0200

    Create search-results-table.component.ts
    Component stub

---
+++ app/search/search-results-table.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'results-table'
+  templateUrl: 'app/search/search-resuts-table.component.html'
+  
+})
+export class SearchTableResultsComponent { }
