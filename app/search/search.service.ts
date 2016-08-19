27f66e3fec6870da2d92e5c282ecfffc1e91426f
Mateusz Koziestański <matico575@gmail.com>
2016-08-19 10:29:15 +0200

    Update search.service.ts

--- app/search/search.service.ts
+++ app/search/search.service.ts
@@ -2,19 +2,19 @@ import { Injectable }   from '@angular/core';
 import {Observable}     from 'rxjs/Rx';
 import {Http, Response} from '@angular/http';
 
-import {IUstawa}        from 'app/ustawa'
+import {IUstawa}        from 'app/ustawa';
 
 @Injectable() 
   export class SearchService {
     private prawoUrl = 'https//: ;
     private testUrl = 'app/sejmometr/page_1.json';
  
-  constructor(private _http:Http) { }
+  constructor(private _http: Http) { }
  
   // Uses http.get() to load a single JSON file
     getSearch(): Observable<any[]> {
     return this.http.get(this.testUrl)
-        .map((response: Response) => <any[]>response.json())
+        .map((response: Response) => <IUstawa[]>response.json())
         .do(data => console.log("Otrzymano: " + JSON.stringify(data)))
         .catch(this.handleError);
   }
