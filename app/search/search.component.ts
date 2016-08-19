876f5c3c220927ddbf177b12c11f2825db357f4f
Mateusz Koziestański <matico575@gmail.com>
2016-08-19 13:07:58 +0200

    Mamma mia
    bordello

--- app/search/search.component.ts
+++ app/search/search.component.ts
@@ -1,7 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
 
 import { SearchFormComponent }    from 'app/search/search-form';
 import { SearchResultsComponent } from 'app/search/search-results';
+import { SearchService }          from 'app/search/search.service';
+
 
 @Component({
  selector: 'search-main',
@@ -11,4 +17,32 @@ import { SearchResultsComponent } from 'app/search/search-results';
   `,
     directives: [SearchFormComponent, SearchResultsComponent]
 })
-export class SearchComponent{ }
+export class SearchComponent{
+  ustawy: Observable<Hero[]>;
+  private _searchTerms = new Subject<string>();
+  
+ constructor(
+ private ustawaSearchService: SearchService,
+ private router: Router){}
+}
+
+ // Push a search term into the observable stream.
+  search(term: string): void {
+    this._searchTerms.next(term);
+  }
+
+   ngOnInit(): void {
+    this.ustawy = this._searchTerms
+      .debounceTime(300)        // wait for 300ms pause in events
+      .distinctUntilChanged()   // ignore if next search term is same as previous
+      .switchMap(term => term   // switch to new observable each time
+        // return the http search observable
+        ? this.ustawaSearchService.search(term)
+        // or the observable of empty heroes if no search term
+        : Observable.of<IUstawa[]>([]))
+      .catch(error => {
+        // TODO: real error handling
+        console.log(error);
+        return Observable.of<IUstawa[]>([]);
+      });
+  }
